f0cb5d5951af26a9686bc0f954483a3d14409d98
Sepehr Akbari <isepehrakbari@gmail.com>
2025-08-14 18:12:11 -0500

    new test sharing

--- src/randomity/evaluate/__init__.py
+++ src/randomity/evaluate/__init__.py
@@ -1,15 +1,3 @@
 from .inspectRandom import inspectRandom
 from .whyRandom import whyRandom
 from .isRandom import isRandom
-
-from .tests import (
-    chisqr_tst,
-    ks_tst,
-    freq_tst,
-    eqdist_tst,
-    gap_tst,
-    serial_tst,
-    permute_tst,
-    entropy_tst,
-    ftt_tst
-)
