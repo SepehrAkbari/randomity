f7eea3466f43bcd805ac61cd4b3ad8ba82596a4e
Sepehr Akbari <isepehrakbari@gmail.com>
2025-08-16 18:14:05 -0500

    fixed version issue in packaging

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Error: Please provide a new version number (./package.sh x.y.z)"
+    echo "Error: invalid version number (./package.sh x.y.z)"
     exit 1
 fi
 
@@ -9,15 +9,11 @@ NEW_VERSION=$1
 PYPROJECT_FILE="pyproject.toml"
 
 echo "Updating version in $PYPROJECT_FILE to $NEW_VERSION..."
-sed -i '' -e "s/^version = \"[0-9.]*\"/version = \"$NEW_VERSION\"/" "$PYPROJECT_FILE"
 
-echo "Cleaning previous build..."
+sed -i '' -e "s/^version = \"[0-9.]*\"/version = \"$NEW_VERSION\"/" "$PYPROJECT_FILE"
 rm -rf dist/*
 
-echo "Building the package..."
 /opt/homebrew/opt/python@3.11/bin/python3.11 -m build
-
-echo "Publishing the package to PyPI..."
 twine upload dist/*
 
 echo "Successfully published version $NEW_VERSION!"
