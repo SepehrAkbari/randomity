1a20984754839095bc69c62b46cae98637cfe6b8
Sepehr Akbari <isepehrakbari@gmail.com>
2025-08-18 13:06:50 -0500

    added gl var

--- src/randomity/evaluate/isRandom.py
+++ src/randomity/evaluate/isRandom.py
@@ -2,7 +2,9 @@ from .._utils.compute_score import _getScore
 from .._utils.gen_test_vector import _gen_test_vector
 from .._utils.check_param import _checkParam_isRandom
 
-def isRandom(sequence: list, threshold: float = 0.6) -> bool:
+SCORE_THRESHOLD = 0.6
+
+def isRandom(sequence: list, threshold: float = SCORE_THRESHOLD) -> bool:
     """
     Returns a boolean based on if a sequence is scored higher than a threshold for the randomness score.
     """
